3a0d449c7235e5da78f8d7669d18e1a8a53ad5e1
James Lamont <james@semblancesystems.com>
2017-12-29 11:58:23 -0500

    Copied Terminal (Session) class from Allison project.

--- lib/bash_rb.rb
+++ lib/bash_rb.rb
@@ -1,5 +1,229 @@
 module BashRb
   class Session
+    attr_reader :process, :handlers
+  
+    SLEEP_TIME = 0.2
+  
+    def self.services 
+      @@services ||= {}
+    end
 
+    def self.define_service(hash)
+      @@services ||= {}
+      @@services = Hash.new(@@services).merge(hash)
+    end
+  
+    def self.get_service(service_name)
+      return nil unless service_name.is_a?(String) || service_name.is_a?(Symbol)
+      (@@services || {})[service_name.to_sym]
+    end
+  
+    def self.define_repl(hash)
+      @@repl_languages ||= {}
+      @@repl_languages = Hash.new(@@repl_languages).merge(hash)
+    end
+  
+    def self.repl_languages
+      if defined?(@@repl_languages)
+        @@repl_languages
+      else
+        {}
+      end
+    end
+  
+    def initialize
+      @response = {}
+      @handlers = [TerminalHandler.new]
+      @process = IO.popen("bash", "w+")
+      Thread.new { handle_process_output }
+    end
+  
+    def close
+      push(current_handler.exit_command) until @handlers.empty?
+    rescue
+    ensure
+      kill_process(@process.pid)
+    end
+  
+    def current_handler
+      @current_handler ||= @handlers.last
+    end
+  
+    def push(command)
+      raise "Terminal closed. You will need to create a new BashRb::Session." if @handlers.empty?
+  
+      if current_handler.is_exiting?(command)
+        @handlers.pop
+        @current_handler = nil
+        push_command(command)
+        nil
+      else
+        command_digest = push_command(command)
+        wait_for_command_output(command_digest)
+        @response.delete(command_digest)
+      end
+    end
+  
+    def repl(command, options = {})
+      repl_handler = Allison::Services::Core::Terminal.repl_languages[options[:language]]
+      raise NotImplementedError.new("Language: #{options[:language]} not implemented") unless repl_handler.is_a?(Hash) && repl_handler[:handler]
+  
+      @handlers << repl_handler[:handler].new
+      @current_handler = nil
+  
+      process.puts(command)
+      process.puts(current_handler.command_delimiter("blank"))
+  
+      self
+    end
+  
+    def load(path)
+      push Allison::Scripts.read(path)
+  
+      self
+    end
+  
+    def method_missing(method_name, *args)
+      current_handler.handle_method_missing(self, method_name, *args)
+    end
+  
+    private
+  
+    def handle_process_output
+      lines = []
+  
+      @process.each_line do |l|
+        l = l.strip
+  
+        if l =~ current_handler.regex
+          @response[$1] = current_handler.prepare_output(lines)
+          lines = []
+        else
+          lines << l.strip
+        end
+      end
+    end
+  
+    def push_command(command)
+      Digest::MD5.hexdigest(command).tap do |command_digest|
+        process.puts(current_handler.prepare_input(command))
+        sleep(SLEEP_TIME)
+        process.puts(current_handler.command_delimiter(command_digest))
+      end
+    end
+  
+    def wait_for_command_output(command_digest)
+      loop do
+        break if @response.has_key?(command_digest)
+        sleep(SLEEP_TIME)
+      end
+    end
+  
+    def kill_process(pid)
+      Process.getpgid(pid)
+      Process.kill("TERM", pid)
+      Process.wait
+      true
+    rescue
+      false
+    end
+  end
+
+  class TerminalHandler
+    def regex
+      @regex ||= /^allison\:terminal\:finish => (.+)$/
+    end
+  
+    def command_delimiter(command_digest)
+      "echo 'allison:terminal:finish => #{command_digest}'"
+    end
+  
+    def prepare_input(str)
+      str
+    end
+  
+    def prepare_output(lines)
+      lines
+    end
+  
+    def handle_method_missing(caller, method_name, *args)
+      options = extract_options!(args)
+  
+      if service_command = extract_service_command(options[:service], method_name)
+        service_command.call(caller, options)
+      elsif options[:service]
+        raise Allison::CommandNotFound.new(["service: #{options[:service]}", method_name])
+      else
+        args.unshift(formatted_flags(options[:flags])) if options[:flags]
+        caller.push("#{method_name} #{args.join(' ')}")
+      end
+    end
+  
+    def exit_command
+      "exit"
+    end
+  
+    def is_exiting?(command)
+      command.strip == exit_command
+    end
+  
+    def extract_options!(args)
+      if args.last.is_a?(Hash)
+        args.pop
+      else
+        {}
+      end
+    end
+  
+    def formatted_flags(flags)
+      if flags.is_a?(Hash)
+        flags.map { |f,v| "-#{f} #{v.to_s}".strip }.join(' ')
+      else
+        flags
+      end
+    end
+  
+    def extract_service_command(service, method)
+      if service_commands = BashRb::Session.get_service(service)
+        service_commands[method.to_sym]
+      end
+    end
+  end
+  
+  class RubyHandler < TerminalHandler
+    def regex
+      @regex ||= /^"allison\:ruby\:finish => (.+)"$/
+    end
+  
+    def command_delimiter(command_digest)
+      "'allison:ruby:finish => #{command_digest}'"
+    end
+  
+    def prepare_input(str)
+      code = <<-EOM
+        require 'json'
+        result = begin
+        #{str}
+        end
+        JSON.dump [result]
+      EOM
+    end
+  
+    def prepare_output(lines)
+      lines.pop # Last element is garbage
+      text = sanitize(lines.last.to_s)
+      JSON.parse(text).first
+    rescue
+      nil
+    end
+  
+    def handle_method_missing(caller, method_name, *args)
+      caller.push "#{method_name}(#{args.to_s[1...-1]})"
+    end
+  
+    def sanitize(text)
+      text.gsub!("\\\"", "\"")
+      text = text[1...-1] # Remove quotes from SSH string
+    end
   end
 end
---
+++ spec/session_spec.rb
@@ -0,0 +1,111 @@
+require 'spec_helper'
+
+RSpec.describe BashRb::Session do
+  subject { BashRb::Session.new }
+
+  describe :push do
+    it "should execute a command" do
+      expect(subject.push("echo 'Testing123'")).to eq(["Testing123"])
+    end
+
+    it "should not cache responses" do
+      expect(subject.instance_variable_get(:@response)).to be_empty
+      expect(subject.push("echo 'Testing123'")).to eq(["Testing123"])
+      expect(subject.instance_variable_get(:@response)).to be_empty
+    end
+  end
+
+  describe :close do 
+    it "should close an opened terminal" do 
+      expect { subject.push("echo test") }.to_not raise_error
+
+      subject.close
+
+      expect { subject.push("echo test") }.to raise_error("Terminal closed. You will need to create a new BashRb::Session.")
+    end
+
+    it "should gracefully handle closing a closed terminal" do 
+      expect { subject.push("echo test") }.to_not raise_error
+
+      Process.kill("TERM", subject.instance_variable_get(:@process).pid)
+      Process.wait
+      
+      expect { subject.close }.to_not raise_error
+      expect { subject.push("echo test") }.to raise_error("Terminal closed. You will need to create a new BashRb::Session.")
+    end
+  end
+
+  # describe :repl do
+  #   it "should raise an error if a language is not supported" do
+  #     expect { subject.repl("irb", language: "ruby") }.to raise_error(NotImplementedError, "Language: ruby not implemented")
+
+  #     BashRb::Session.define_repl(
+  #       "ruby" => { handler: Allison::Services::Core::RubyHandler }
+  #     )
+
+  #     expect { subject.repl("irb", language: "ruby") }.to_not raise_error
+  #   end
+
+  #   it "should return Terminal object" do
+  #     BashRb::Session.define_repl(
+  #       "ruby" => { handler: Allison::Services::Core::RubyHandler }
+  #     )
+
+  #     expect(subject.repl("irb", language: "ruby")).to eq(subject)
+  #   end
+
+  #   context "ruby" do
+  #     before do
+  #       BashRb::Session.define_repl(
+  #         "ruby" => { handler: Allison::Services::Core::RubyHandler }
+  #       )
+  #     end
+
+  #     after { subject.close }
+
+  #     it "should execute ruby code" do
+  #       subject.repl("irb", language: "ruby")
+  #       expect(subject.push("1 + 1")).to eq(2)
+  #     end
+  #   end
+  # end
+
+  describe "dynamic commands" do
+    it "should pass basic commands to push" do
+      expect(subject).to receive(:push).with("echo 'Testing123'")
+      subject.echo("'Testing123'")
+    end
+
+    it "should pass in commands with flags" do
+      expect(subject).to receive(:push).with("ls -alh")
+      subject.ls("-alh")
+    end
+
+    it "should pass in commands with flags via options" do
+      expect(subject).to receive(:push).with("ls -a -l -h")
+      subject.ls(flags: {a: nil, l: nil, h: nil})
+
+      expect(subject).to receive(:push).with("ssh -i ~/.ssh/x.pem a@b.com")
+      subject.ssh("a@b.com", flags: {i: "~/.ssh/x.pem"})
+
+      expect(subject).to receive(:push).with("cx ssh -s some_app console")
+      subject.cx("ssh", "-s some_app console")
+    end
+
+    # describe "#define_service" do
+    #   it "should allow you define a shortcut for a command" do
+    #     BashRb::Session.define_service(
+    #       some_app: {
+    #         ssh: lambda { |term, options| term.cx("ssh -s abc machine_name") }
+    #       }
+    #     )
+    #     expect(subject).to receive(:push).with("cx ssh -s abc machine_name")
+    #     subject.ssh(service: "some_app")
+    #   end
+
+    #   it "should raise Allison::CommandNotFound when service doesn't exist" do
+    #     expect { subject.ssh(service: "blah") }.to raise_error(Allison::CommandNotFound)
+    #   end
+    # end
+  end
+end
